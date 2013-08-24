df89ec6e58e2681662e39ea5f8a142a02db13f1f
Tim Abell <tim@timwise.co.uk>
2013-08-24 21:16:41 +0100

    Fix gnome stupidity

---
+++ bin/fix-stupid-gnome-settings.sh
@@ -0,0 +1,3 @@
+# ffs https://help.gnome.org/users/gnome-help/3.8/power-closelid.html.en
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action blank
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action blank
