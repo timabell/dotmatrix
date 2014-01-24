ec39f4cc9e326e03e966283035ccfea88c95a480
Tim Abell <tim@timwise.co.uk>
2014-01-24 00:02:35 +0000

    Mint 16 package installation. Remove firefox.

--- bin/packages.sh
+++ bin/packages.sh
@@ -1,2 +1,7 @@
-#!/bin/sh -v
+#!/bin/sh
+echo removing firefox
+echo expect a sudo prompt
+sudo apt-get remove firefox
+echo "installing packages:"
+cat packages.txt
 sudo apt-get install `cat packages.txt`
