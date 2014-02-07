383605f679a12e7421e64e943fd9180589d1b37a
Tim Abell <tim@timwise.co.uk>
2014-02-07 00:02:40 +0000

    Allow one at a time packages.
    Run update first

--- bin/packages.sh
+++ bin/packages.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+sudo apt-get update
 echo removing firefox
 echo expect a sudo prompt
 sudo apt-get remove firefox
-echo "installing packages:"
-cat packages.txt
-sudo apt-get install `cat packages.txt`
+if [ "$1" = "single" ]; then
+  for x in `cat packages.txt`; do
+    echo "installing packages $x"
+    sudo apt-get install $x
+  done
+else
+  echo "installing packages:"
+  cat packages.txt
+  sudo apt-get install `cat packages.txt`
+fi
