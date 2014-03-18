603b5be50f362d56b3e7fe86f48f4e84b919f1a0
Tim Abell <tim@timwise.co.uk>
2014-03-18 23:39:58 +0000

    Vim shell wrapper - expand aliases

--- .vim/shell-wrapper.sh
+++ .vim/shell-wrapper.sh
@@ -3,6 +3,10 @@
 # vim doesn't clear previous commands so clear previous output now:
 clear
 
+# import aliases
+shopt -s expand_aliases
+[ ! -f "$HOME/.bash_aliases" ] || . "$HOME/.bash_aliases"
+
 # strip the -c that vim adds to bash from arguments to this script
 shift
 
@@ -10,4 +14,4 @@ shift
 echo \$ $@
 
 # run the command:
-bash -c "$@"
+eval $@
