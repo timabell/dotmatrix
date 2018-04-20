86cae188b3d29f3a2627e2db8cb7b12894e9359c
Tim Abell <tim@timwise.co.uk>
2018-04-20 23:40:22 +0100

    Bootstrap instructions

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -v
+# run this with:
+# curl https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/bootstrap.sh | sh
 wget https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/packages.txt
 wget https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/packages.sh
 chmod +x packages.sh
