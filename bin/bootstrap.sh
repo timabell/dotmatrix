a95046b5a7e35bb6780159100e83163ce570ad11
Tim Abell <tim@timwise.co.uk>
2018-04-21 01:47:25 +0100

    bootstrap iteration

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -v
 # run this with:
 # curl https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/bootstrap.sh | sh
-wget https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/packages.txt
-wget https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/packages.sh
+wget -N https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/packages.txt
+wget -N https://raw.githubusercontent.com/timabell/dotmatrix/master/bin/packages.sh
 chmod +x packages.sh
 ./packages.sh
--- bin/packages.sh
+++ bin/packages.sh
@@ -3,21 +3,22 @@ echo Expect a sudo prompt for apt
 
 # http://www.webupd8.org/2014/09/syncthing-gui-gtk3-python-gui-ubuntu-ppa.html
 sudo add-apt-repository -y ppa:nilarimogard/webupd8
-sudo apt-get update
+sudo apt update
 
 # echo removing firefox
 # sudo apt-get remove firefox
 
+echo removing unwanted packages
+sudo apt remove keepassx
+
 if [ "$1" = "single" ]; then
   for x in `cat packages.txt`; do
     echo "installing packages $x"
-    sudo apt-get install $x
+    sudo apt install $x
   done
 else
   echo "installing packages:"
   cat packages.txt
-  sudo apt-get install `cat packages.txt`
+  sudo apt install `cat packages.txt`
 fi
 
-echo removing unwanted packages
-sudo apt remove keepassx
