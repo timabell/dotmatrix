7c8bdc035f0e4a6aa82aaec4bb748654528caf8a
Tim Abell <tim@timwise.co.uk>
2019-09-02 14:39:04 +0100

    Install script for az
    On linux mint 19.2

---
+++ bin/install-az.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -v
+
+# https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-apt?view=azure-cli-latest
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor | \
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
+#AZ_REPO=$(lsb_release -cs)
+AZ_REPO=bionic
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-get update
+sudo apt-get install azure-cli
+
+az --version
