7c64399fe500f887efd5a5335031d30ab069bc19
Tim Abell <tim@timwise.co.uk>
2021-04-16 11:04:26 +0100

    Add gh-cli

---
+++ bin/install-gh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -v
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
