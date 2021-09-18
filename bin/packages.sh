0a344c8222b1c7ac6d0bef1e9c89529c48121923
Tim Abell <tim@timwise.co.uk>
2021-09-18 01:14:36 +0100

    Updated shutter install via PPA

--- bin/packages.sh
+++ bin/packages.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -v
 echo Expect a sudo prompt for apt
 
-# http://www.webupd8.org/2014/09/syncthing-gui-gtk3-python-gui-ubuntu-ppa.html
-sudo add-apt-repository -y ppa:nilarimogard/webupd8
-sudo apt update
-
-# echo removing firefox
-# sudo apt-get remove firefox
+sudo add-apt-repository -y ppa:nilarimogard/webupd8 # http://www.webupd8.org/2014/09/syncthing-gui-gtk3-python-gui-ubuntu-ppa.html
+sudo add-apt-repository ppa:shutter/ppa # https://www.linuxuprising.com/2021/08/official-shutter-screenshot-tool.html
+sudo apt update # this is no longer needed on Ubuntu but its required on Linux Mint
 
 echo removing unwanted packages
 sudo apt remove keepassx
