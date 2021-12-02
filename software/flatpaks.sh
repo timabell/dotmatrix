9f5adf34bfaeee059b0309a0b650db1f688cfe5d
Tim Abell <tim@timwise.co.uk>
2021-12-02 11:35:59 +0000

    Add tor launcher
    Finally something that works!

--- software/flatpaks.sh
+++ software/flatpaks.sh
@@ -4,3 +4,4 @@ flatpak install -y flathub com.slack.Slack
 flatpak install -y flathub org.signal.Signal
 flatpak install -y flathub us.zoom.Zoom
 flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y com.github.micahflee.torbrowser-launcher
