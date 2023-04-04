ce2dcd6848c6026faf54f51c3c5e24194a51e934
Tim Abell <tim@timwise.co.uk>
2023-04-04 12:09:02 +0100

    Add zellij installer
    https://github.com/zellij-org/zellij

---
+++ software/zellij.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -vx
+
+cd ~/bin
+wget https://github.com/zellij-org/zellij/releases/download/v0.35.2/zellij-x86_64-unknown-linux-musl.tar.gz
+tar -xzvf zellij-x86_64-unknown-linux-musl.tar.gz
+rm zellij-x86_64-unknown-linux-musl.tar.gz
