38f0394fc2a6513f08d4ef1710473203715dda8d
Tim Abell <tim@timwise.co.uk>
2024-07-28 15:58:49 +0100

    git-for-windows in msys

--- software/pacman.sh
+++ software/pacman.sh
@@ -1,23 +1,32 @@
 #!/bin/sh -v
+
 # https://dev.to/shrihankp/make-your-terminal-look-swag-with-msys2-and-zsh-32b7
-pacman -S man vim openssh
+
+# search:
+#   pacman -Ss [name]
+#   https://www.msys2.org/docs/package-management/#finding-a-package
+
+### git-for-windows setup because: https://github.com/msys2/MSYS2-packages/issues/3358
+# https://github.com/git-for-windows/git/wiki/Install-inside-MSYS2-proper
+
+sed -i '/^\[mingw32\]/{ s|^|[git-for-windows]\nServer = https://wingit.blob.core.windows.net/x86-64\n\n[git-for-windows-mingw32]\nServer = https://wingit.blob.core.windows.net/i686\n\n|; }' /etc/pacman.conf
+
+rm -r /etc/pacman.d/gnupg/
+pacman-key --init
+pacman-key --populate msys2
 
 curl -L https://raw.githubusercontent.com/git-for-windows/build-extra/HEAD/git-for-windows-keyring/git-for-windows.gpg |
 pacman-key --add - &&
-pacman-key --lsign-key 91883E11E83DC29D14104DB4EDD44359093056EE
+pacman-key --lsign-key E8325679DFFF09668AD8D7B67115A57376871B1C &&
+pacman-key --lsign-key 3B6D86A1BA7701CD0F23AED888138B9E1A9F3986
 
 pacman -Syyuu
-pacman -Syuu
 
-pacman -S git git-extras
-pacman -S mingw-w64-x86_64-git-credential-manager
-# pacman -S mingw-w64-x86_64-git-lfs # not needed
-# pacman -S mingw-w64-x86_64-git-doc-man mingw-w64-x86_64-git-doc-html # failed
+########## expect shell restart ##########
 
-pacman -S zsh
-pacman -S mingw-w64-x86_64-fzf
-pacman -S mingw-w64-x86_64-delta
-pacman -S msys/tig
+pacman -Suu
 
-# search - https://www.msys2.org/docs/package-management/#finding-a-package
-# pacman -Ss [name]
+### end of git-for-windows setup
+
+pacman -S man nvim openssh zsh mingw-w64-x86_64-fzf mingw-w64-x86_64-delta msys/tig
+
