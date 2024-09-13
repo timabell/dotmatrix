fa5a13833f5a90f8b7530a7b05c2f2d0afcc2869
Tim Abell <tim@timwise.co.uk>
2024-09-13 15:52:35 +0100

    Sort winget list

--- software/winget.sh
+++ software/winget.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -v
+winget install GitExtensionsTeam.GitExtensions
 winget install GnuWin32.Tree
 winget install JFLarvoire.Ag
 winget install JetBrains.Toolbox
-winget install dandavison.delta
-winget install Mozilla.Firefox
-winget install Microsoft.VisualStudioCode
-winget install MSYS2.MSYS2
-winget install GitExtensionsTeam.GitExtensions
-winget install Microsoft.PowerToys
 winget install KDE.KDiff3
+winget install MSYS2.MSYS2
+winget install Microsoft.PowerToys
+winget install Microsoft.VisualStudioCode
+winget install Mozilla.Firefox
 winget install ScooterSoftware.BeyondCompare5
+winget install dandavison.delta
