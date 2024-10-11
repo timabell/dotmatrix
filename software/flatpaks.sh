1510c3940104525281e50d70bc20404647252cef
Tim Abell <tim@timwise.co.uk>
2024-10-11 14:30:50 +0100

    Add Azure Storage Explorer flatpack

--- software/flatpaks.sh
+++ software/flatpaks.sh
@@ -2,9 +2,10 @@
 flatpak install -y codes.merritt.FeelingFinder
 flatpak install -y com.github.micahflee.torbrowser-launcher
 flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y com.logseq.Logseq
+flatpak install -y com.microsoft.AzureStorageExplorer
 flatpak install -y flathub com.slack.Slack
 flatpak install -y flathub org.signal.Signal
 flatpak install -y flathub org.telegram.desktop
 flatpak install -y flathub us.zoom.Zoom
 flatpak install -y org.inkscape.Inkscape
-flatpak install -y com.logseq.Logseq
