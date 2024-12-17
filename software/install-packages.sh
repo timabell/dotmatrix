300c688d0332ccfd9d292b025080228b74c6621d
Tim Abell <tim@timwise.co.uk>
2024-12-17 10:46:13 +0000

    Remove outdated ppas
    Not supported on noble
    Syncthing gtk avaiable without it.

--- software/install-packages.sh
+++ software/install-packages.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -v
 echo "Expect a sudo prompt for apt"
 
-sudo add-apt-repository -y ppa:nilarimogard/webupd8 # http://www.webupd8.org/2014/09/syncthing-gui-gtk3-python-gui-ubuntu-ppa.html
-sudo add-apt-repository -y ppa:shutter/ppa # https://www.linuxuprising.com/2021/08/official-shutter-screenshot-tool.html
-sudo apt update # this is no longer needed on Ubuntu but its required on Linux Mint
-
 echo removing unwanted packages
 sudo apt remove keepassx
 
