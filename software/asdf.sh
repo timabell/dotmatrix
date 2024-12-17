b09f98004ee3f44fe820ac87fe6125da049ce94d
Tim Abell <tim@timwise.co.uk>
2024-12-17 10:08:58 +0000

    Add more asdf plugins

--- software/asdf.sh
+++ software/asdf.sh
@@ -5,3 +5,8 @@
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 
 asdf plugin add dotnet-core
+asdf plugin add golang
+asdf plugin add nodejs
+asdf plugin add python
+asdf plugin add ruby
+asdf plugin add rust
