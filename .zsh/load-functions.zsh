e6a943c6b53daef3362240e59591ededfa8642cf
Tim Abell <t@0x5.uk>
2025-02-04 15:31:41 +0000

    add ggl

--- .zsh/load-functions.zsh
+++ .zsh/load-functions.zsh
@@ -1,7 +1,6 @@
 # to be sourced in .zshrc
 
-autoload -U ff
-autoload -U ffe
-autoload -U gdq
-autoload -U git-brmv
-autoload -U gx
+# autoload all functions in ~/.zsh/functions
+for func in ~/.zsh/functions/*; do
+  autoload -U ${func:t}
+done
