4286f0a27c6df35a838afe4500c9cdc12f45b333
Tim Abell <t@0x5.uk>
2025-06-09 21:25:11 +0100

    Add syncthing installer
    Default apt is way out of date

---
+++ software/syncthing.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -v
+# https://apt.syncthing.net/
+
+# Add the release PGP keys:
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Add the "candidate" channel to your APT sources:
+#echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
