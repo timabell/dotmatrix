78386582cb829273af3c25cf325fbf08800f1cfe
Tim Abell <t@0x5.uk>
2025-08-07 23:07:17 +0100

    Update symlink setup

--- bin/symlinks.sh
+++ bin/symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -v
 cd ~
 ln -s ~/Documents/programs/bin
-ln -s ~/Phone/vimwiki notes
+ln -s ~/oneplus9-home/Documents/looksyk/pages notes
 ln -s ~/repo/dotmatrix dm
 ln -s ~/repo/scripts
+ln -s ~/repo/0x5.uk blog
