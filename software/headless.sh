7a716293395a07cf0bde859524a34186cbfeaa54
Tim Abell <t@0x5.uk>
2025-09-03 23:22:10 +0100

    Headless server setup - add ups config

--- software/headless.sh
+++ software/headless.sh
@@ -1,2 +1,16 @@
+#!/bin/sh -v
+
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-a-remote-desktop-with-x2go-on-ubuntu-20-04
-sudo apt install mint-meta-mate x2goserver x2goserver-xsession
+# https://help.ubuntu.com/community/apcupsd
+
+sudo apt install mint-meta-mate x2goserver x2goserver-xsession apcupsd
+
+APC_CONF_SOURCE="$HOME/Documents/config/apcupsd.conf"
+APC_CONF="/etc/apcupsd/apcupsd.conf"
+APC_CONF_BAK="${APC_CONF}.bak"
+
+# check for .bak for idempotency
+if [ ! -f "$APC_CONF_BAK" ]; then
+    sudo cp "$APC_CONF" "$APC_CONF_BAK"
+    sudo cp "$APC_CONF_SOURCE" "$APC_CONF"
+fi
