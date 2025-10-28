cdc8f70fa75303d853abdaceb7eb75ffcfa03ad7
Tim Abell <t@0x5.uk>
2025-10-28 14:55:47 +0000

    Make openvpn sh idempotent
    Windsurf/claude

--- software/openvpn3.sh
+++ software/openvpn3.sh
@@ -1,26 +1,31 @@
-#!/bin/sh -v
+#!/bin/sh
 set -e
 
 # needed for saml auth
 # because networkmanager can't do it https://gitlab.gnome.org/GNOME/NetworkManager-openvpn/-/issues/150
 
 # https://community.openvpn.net/Pages/OpenVPN3Linux#stable-repository-debian-ubuntu
+# modified to be idempotent
 
+echo "Installing prerequisites..."
 apt install apt-transport-https curl
 
-# Retrieve the OpenVPN Inc package signing key:
+if [ ! -f /etc/apt/keyrings/openvpn.asc ]; then
+    echo "Downloading OpenVPN signing key..."
+    curl -sSfL https://packages.openvpn.net/packages-repo.gpg > /etc/apt/keyrings/openvpn.asc
+else
+    echo "OpenVPN signing key already exists"
+fi
 
-# mkdir -p /etc/apt/keyrings    ### This might not exist in all distributions
-curl -sSfL https://packages.openvpn.net/packages-repo.gpg >/etc/apt/keyrings/openvpn.asc
+if ! grep -q "openvpn3" /etc/apt/sources.list.d/openvpn3.list 2>/dev/null; then
+    echo "Adding OpenVPN3 repository..."
+    echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian noble main" >> /etc/apt/sources.list.d/openvpn3.list
+    apt update
+else
+    echo "OpenVPN3 repository already configured"
+fi
 
-# Replace the DISTRIBUTION part in the command below using the release name from the table above to set up the apt source listing:
-# echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian DISTRIBUTION main" >>/etc/apt/sources.list.d/openvpn3.list
-
-# Example for Debian 12:
-
-echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian noble main" >> /etc/apt/sources.list.d/openvpn3.list
-
-# To install OpenVPN 3 Linux, run these commands:
-
-apt update
+echo "Installing OpenVPN3..."
 apt install openvpn3
+
+echo "OpenVPN3 installation complete"
