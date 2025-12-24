3a23b6fe0931a419564d9e4f3750150c0c12f7b7
Tim Abell <t@0x5.uk>
2025-12-24 18:09:32 +0000

    Add cargo binstall

---
+++ software/cargo-binstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -v
+# https://github.com/cargo-bins/cargo-binstall
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
